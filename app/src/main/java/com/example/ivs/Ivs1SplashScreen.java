ed6b2f0e250d2e83d84e39e62889a1256d55bdd7
rupads <ruparani5055@gmail.com>
2021-12-21 03:20:49 +0600

    Inven-Men-Sys App

--- app/src/main/java/com/example/ivs/Ivs1SplashScreen.java
+++ app/src/main/java/com/example/ivs/Ivs1SplashScreen.java
@@ -5,15 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.window.SplashScreen;
 
 public class Ivs1SplashScreen extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private int progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_ivs1splashscreen);
 
-        getSupportActionBar().hide();
+        progressBar = (ProgressBar) findViewById(R.id.progrssbarId);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                startApp();
+            }
+        });
+
+        thread.start();
+    }
+    public void doWork(){
+        for(progress=20;progress<=100;progress = progress+20){
+            try{
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        }
+
+    public void startApp(){
+        Intent intent = new Intent(Ivs1SplashScreen.this,Ivs2AccountPage.class);
+        startActivity(intent);
+        finish();
     }
 }
